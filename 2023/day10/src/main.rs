0f294488de290e97fbc2fcbd73a7867e18322dea
Peter Radziewicz <peterradziewicz@gmail.com>
2023-12-10 13:08:14 -0500

    some testing and more implementation

--- 2023/day10/src/main.rs
+++ 2023/day10/src/main.rs
@@ -9,7 +9,7 @@ SJLL7
 LJ.LJ\
 ";
 
-#[derive(Debug, PartialEq, Clone, Copy)]
+#[derive(Debug, PartialEq, Eq, Clone, Copy)]
 enum PipeKind {
     Vertical,   // |
     Horizontal, // -
@@ -24,8 +24,8 @@ enum PipeKind {
 impl PipeKind {
     fn from(c: char) -> Self {
         match c {
-            '|' => PipeKind::Horizontal,
-            '-' => PipeKind::Vertical,
+            '|' => PipeKind::Vertical,
+            '-' => PipeKind::Horizontal,
             'L' => PipeKind::NorthEast,
             'J' => PipeKind::NorthWest,
             '7' => PipeKind::SouthWest,
@@ -37,15 +37,25 @@ impl PipeKind {
     }
 }
 
-#[derive(Debug)]
+#[derive(Debug, PartialEq, Eq)]
 struct Pipe {
     row: usize,
     column: usize,
     kind: PipeKind,
 }
 
+impl Pipe {
+    fn from(row: usize, column: usize, c: char) -> Self {
+        Self {
+            row,
+            column,
+            kind: PipeKind::from(c),
+        }
+    }
+}
+
 fn main() {
-    part_1(TEST);
+    part_1(INPUT);
 }
 
 fn deserialize(input: &str) -> Vec<Vec<Pipe>> {
@@ -57,11 +67,7 @@ fn deserialize(input: &str) -> Vec<Vec<Pipe>> {
         .map(|(row, l)| {
             l.chars()
                 .enumerate()
-                .map(|(column, c)| Pipe {
-                    kind: PipeKind::from(c),
-                    row,
-                    column,
-                })
+                .map(|(column, c)| Pipe::from(row, column, c))
                 .collect()
         })
         .collect();
@@ -74,11 +80,9 @@ fn deserialize(input: &str) -> Vec<Vec<Pipe>> {
 
 fn part_1(input: &str) -> u64 {
     let pipes = deserialize(input);
-
     let mut current_pipe = &pipes[0][0];
 
     let mut row_idx = 0;
-
     while row_idx < pipes.len() {
         let row = &pipes[row_idx];
         let mut col_idx = 0;
@@ -93,16 +97,50 @@ fn part_1(input: &str) -> u64 {
         row_idx += 1;
     }
 
-    println!("{:?}", current_pipe);
+    assert_eq!(current_pipe.kind, PipeKind::Start);
+
+    println!("{:?}", pipes[current_pipe.row - 1][current_pipe.column + 1]);
+
+    let mut path_1: Vec<&Pipe> = vec![current_pipe];
+    let mut path_2: Vec<&Pipe> = vec![current_pipe];
 
     0
 }
 
-
 #[cfg(test)]
 mod tests {
+    use super::*;
+
     #[test]
     fn test_deser() {
-        
+        let test = "\
+|-LJ7F.S
+S.F7JL-|\
+";
+
+        let expected: Vec<Vec<Pipe>> = vec![
+            vec![
+                Pipe::from(0, 0, '|'),
+                Pipe::from(0, 1, '-'),
+                Pipe::from(0, 2, 'L'),
+                Pipe::from(0, 3, 'J'),
+                Pipe::from(0, 4, '7'),
+                Pipe::from(0, 5, 'F'),
+                Pipe::from(0, 6, '.'),
+                Pipe::from(0, 7, 'S'),
+            ],
+            vec![
+                Pipe::from(1, 0, 'S'),
+                Pipe::from(1, 1, '.'),
+                Pipe::from(1, 2, 'F'),
+                Pipe::from(1, 3, '7'),
+                Pipe::from(1, 4, 'J'),
+                Pipe::from(1, 5, 'L'),
+                Pipe::from(1, 6, '-'),
+                Pipe::from(1, 7, '|'),
+            ],
+        ];
+
+        assert_eq!(deserialize(test), expected)
     }
 }
