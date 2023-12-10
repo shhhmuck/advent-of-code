69266da9b234419dc651416965c1ea59a9ccdd57
Peter Radziewicz <peterradziewicz@gmail.com>
2023-12-10 12:09:16 -0500

    LATEST

--- 2023/day10/src/main.rs
+++ 2023/day10/src/main.rs
@@ -1,8 +1,15 @@
 use std::time::Instant;
 
 const INPUT: &str = include_str!("./input.txt");
+const TEST: &str = "\
+7-F7-
+.FJ|7
+SJLL7
+|F--J
+LJ.LJ\
+";
 
-#[derive(Debug, PartialEq)]
+#[derive(Debug, PartialEq, Clone, Copy)]
 enum PipeKind {
     Vertical,   // |
     Horizontal, // -
@@ -38,21 +45,64 @@ struct Pipe {
 }
 
 fn main() {
-    part_1(INPUT);
+    part_1(TEST);
 }
 
-fn part_1(input: &str) -> u64 {
+fn deserialize(input: &str) -> Vec<Vec<Pipe>> {
     let start = Instant::now();
 
-    let pipes: Vec<Vec<PipeKind>> = input
+    let pipes: Vec<Vec<Pipe>> = input
         .lines()
-        .map(|l| l.chars().map(PipeKind::from).collect())
+        .enumerate()
+        .map(|(row, l)| {
+            l.chars()
+                .enumerate()
+                .map(|(column, c)| Pipe {
+                    kind: PipeKind::from(c),
+                    row,
+                    column,
+                })
+                .collect()
+        })
         .collect();
 
     let end = Instant::now();
     println!("Deserialized in {:?}", end.duration_since(start));
 
-    println!("{:?}", pipes[4][77]);
+    pipes
+}
+
+fn part_1(input: &str) -> u64 {
+    let pipes = deserialize(input);
+
+    let mut current_pipe = &pipes[0][0];
+
+    let mut row_idx = 0;
+
+    while row_idx < pipes.len() {
+        let row = &pipes[row_idx];
+        let mut col_idx = 0;
+        while col_idx < row.len() {
+            let pipe = &row[col_idx];
+            if pipe.kind == PipeKind::Start {
+                current_pipe = pipe;
+                break;
+            }
+            col_idx += 1;
+        }
+        row_idx += 1;
+    }
+
+    println!("{:?}", current_pipe);
 
     0
+}
+
+
+#[cfg(test)]
+mod tests {
+    #[test]
+    fn test_deser() {
+        
+    }
 }
