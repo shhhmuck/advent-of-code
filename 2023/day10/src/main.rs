bb39b1f3d2b4af643e0fa011253a3eab797ed21a
Peter Radziewicz <peterradziewicz@gmail.com>
2023-12-11 23:37:21 -0500

    wip algorithm and visualization files

--- 2023/day10/src/main.rs
+++ 2023/day10/src/main.rs
@@ -9,6 +9,19 @@ SJLL7
 LJ.LJ\
 ";
 
+const TEST2: &str = "\
+FF7FSF7F7F7F7F7F---7
+L|LJ||||||||||||F--J
+FL-7LJLJ||||||LJL-77
+F--JF--7||LJLJ7F7FJ-
+L---JF-JLJ.||-FJLJJ7
+|F|F-JF---7F7-L7L|7|
+|FFJF7L7F-JF7|JL---7
+7-L-JL7||F7|L7F-7F7|
+L.L7LFJ|||||FJL7||LJ
+L7JLJL-JLJLJL--JLJ.L\
+";
+
 #[derive(Debug, PartialEq, Eq, Clone, Copy)]
 enum Direction {
     North,
@@ -41,6 +54,19 @@ impl PipeKind {
             '.' => PipeKind::Ground,
             'S' => PipeKind::Start,
             _ => unreachable!("All characters in the input are handled"),
+        }
+    }
+
+    fn as_char(&self) -> char {
+        match self {
+            PipeKind::Vertical => '|',
+            PipeKind::Horizontal => '-',
+            PipeKind::NorthEast => '┕',
+            PipeKind::NorthWest => '┙',
+            PipeKind::SouthWest => '┑',
+            PipeKind::SouthEast => '┍',
+            PipeKind::Ground => '.',
+            PipeKind::Start => 'S',
         }
     }
 
@@ -103,7 +129,9 @@ impl Pipe {
 }
 
 fn main() {
-    let solve = part_1(INPUT);
+    // let solve = part_1(INPUT);
+    // println!("Answer: {}", solve);
+    let solve = part_2(INPUT);
     println!("Answer: {}", solve);
 }
 
@@ -128,7 +156,7 @@ fn deserialize(input: &str) -> Vec<Vec<Pipe>> {
 }
 
 fn part_1(input: &str) -> u64 {
-    let pipes = deserialize(input);
+    let mut pipes = deserialize(input);
 
     let mut current_pipe = &pipes[0][0];
     let mut row_idx = 0;
@@ -213,6 +241,218 @@ fn part_1(input: &str) -> u64 {
     (main_loop.len() / 2) as u64
 }
 
+fn part_2(input: &str) -> u64 {
+    let mut pipes = deserialize(input);
+
+    let mut current_pipe = &pipes[0][0];
+    let mut row_idx = 0;
+
+    while row_idx < pipes.len() {
+        let row = &pipes[row_idx];
+        let mut col_idx = 0;
+        while col_idx < row.len() {
+            let pipe = &row[col_idx];
+            if pipe.kind == PipeKind::Start {
+                current_pipe = pipe;
+                break;
+            }
+            col_idx += 1;
+        }
+        row_idx += 1;
+    }
+
+    let start = Instant::now();
+    let mut main_loop: Vec<&Pipe> = vec![current_pipe];
+
+    // logic to start the main loop
+    for dir in current_pipe.kind.can_travel() {
+        match dir {
+            Direction::North => {
+                if current_pipe.row != 0 {
+                    let p = &pipes[current_pipe.row - 1][current_pipe.column];
+                    if p.kind.can_travel().contains(&Direction::South) {
+                        main_loop.push(p);
+                        break;
+                    }
+                }
+            }
+            Direction::East => {
+                let c = current_pipe.column + 1;
+                if c < pipes[current_pipe.row].len() {
+                    let p = &pipes[current_pipe.row][c];
+                    if p.kind.can_travel().contains(&Direction::West) {
+                        main_loop.push(p);
+                        break;
+                    }
+                }
+            }
+            Direction::South => {
+                let r = current_pipe.row + 1;
+                if r < pipes.len() {
+                    let p = &pipes[r][current_pipe.column];
+                    if p.kind.can_travel().contains(&Direction::North) {
+                        main_loop.push(p);
+                        break;
+                    }
+                }
+            }
+            Direction::West => {
+                if current_pipe.column != 0 {
+                    let p = &pipes[current_pipe.row][current_pipe.column - 1];
+                    if p.kind.can_travel().contains(&Direction::East) {
+                        main_loop.push(p);
+                        break;
+                    }
+                }
+            }
+        }
+    }
+
+    loop {
+        let p = main_loop[main_loop.len() - 1];
+        if p.is_start {
+            break;
+        }
+        let n = p.walk(main_loop[main_loop.len() - 2], &pipes);
+        main_loop.push(n);
+    }
+
+    // println!("loop 1: {:?}", main_loop.len() / 2);
+
+    // Paint the loop with ~
+
+    let mut enclosed_count = 0;
+
+    for row in pipes.iter() {
+        let mut is_inside = false;
+
+        for cur_pipe in row {
+            if main_loop.contains(&cur_pipe) {
+                is_inside = !is_inside;
+
+                continue;
+            }
+
+            // println!(
+            //     "({},{}) is inside: {is_inside}",
+            //     cur_pipe.row, cur_pipe.column
+            // );
+
+            if cur_pipe.row == 0 || cur_pipe.row == pipes.len() - 1
+                || cur_pipe.column == 0 || cur_pipe.column == pipes[cur_pipe.row].len() - 1
+            {
+                // println!("edge: {:?}", cur_pipe);
+                continue;
+            }
+
+            if is_inside {
+                enclosed_count += 1;
+            }
+        }
+    }
+
+    let end = Instant::now();
+    println!("Processed in {:?}\n", end.duration_since(start));
+
+    
+
+    let mut all: Vec<Vec<char>> = input.lines().map(|l| l.chars().collect()).collect();
+
+    for (r_idx, r) in all.iter_mut().enumerate() {
+        for (c_idx, c) in  r.iter_mut().enumerate() {
+            if !main_loop.contains(&&Pipe { row: r_idx, column: c_idx, kind: PipeKind::from(*c), is_start: PipeKind::from(*c) ==PipeKind::Start}) {
+                *c = ' ';
+            }
+        }
+    }
+
+    for pipe in &main_loop {
+        all[pipe.row][pipe.column] = PipeKind::from(all[pipe.row][pipe.column]).as_char();
+    };
+
+    let mut s: String = String::new();
+
+    for l in all {
+        for c in l {
+            s.push(c);
+        }
+        s.push('\n');
+    }
+
+    println!("{}", s);
+
+    enclosed_count
+
+    // for (row_idx, row) in all.iter().enumerate() {
+    //     for (col_idx, col) in row.iter().enumerate() {
+    //         // println!("{:?}", all[row_idx][col_idx]);
+
+    //         // is main loop
+    //         if *col == '0' {
+    //             continue;
+    //         }
+    //         // edges can't be enclosed
+    //         if row_idx == 0
+    //             || row_idx == all.len() - 1
+    //             || col_idx == 0
+    //             || col_idx == all[row_idx].len() - 1
+    //         {
+    //             continue;
+    //         }
+
+    //         let mut escaped = false;
+
+    //         // north
+    //         for i in (0..=row_idx - 1).rev() {
+    //             if all[i][col_idx] == '0' {
+    //                 break;
+    //             }
+    //             escaped = true;
+    //         }
+    //         if !escaped {
+    //             continue;
+    //         }
+
+    //         // south
+    //         for i in row_idx + 1..all.len() {
+    //             if all[i][col_idx] == '0' {
+    //                 break;
+    //             }
+    //             escaped = true;
+    //         }
+    //         if !escaped {
+    //             continue;
+    //         }
+
+    //         // east
+    //         for i in col_idx + 1..all[row_idx].len() {
+    //             if all[row_idx][i] == '0' {
+    //                 break;
+    //             }
+    //             escaped = true;
+    //         }
+    //         if !escaped {
+    //             continue;
+    //         }
+
+    //         // west
+    //         for i in (0..=col_idx - 1).rev() {
+    //             if all[row_idx][i] == '0' {
+    //                 break;
+    //             }
+    //             escaped = true;
+    //         }
+    //         if !escaped {
+    //             continue;
+    //         }
+    //     }
+
+    //     enclosed += 1;
+    // }
+
+
+}
+
 #[cfg(test)]
 mod tests {
     use super::*;
@@ -274,5 +514,52 @@ LJ...\
 ";
 
         assert_eq!(part_1(input), 8)
+    }
+
+    #[test]
+    fn test_part_2_small() {
+        let input = "\
+.F----7F7F7F7F-7....
+.|F--7||||||||FJ....
+.||.FJ||||||||L7....
+FJL7L7LJLJ||LJ.L-7..
+L--J.L7...LJS7F-7L7.
+....F-J..F7FJ|L7L7L7
+....L7.F7||L7|.L7L7|
+.....|FJLJ|FJ|F7|.LJ
+....FJL-7.||.||||...
+....L---J.LJ.LJLJ...\
+";
+
+        assert_eq!(part_2(input), 8)
+    }
+
+    #[test]
+    fn test_part_2_big() {
+        let input = "\
+FF7FSF7F7F7F7F7F---7
+L|LJ||||||||||||F--J
+FL-7LJLJ||||||LJL-77
+F--JF--7||LJLJ7F7FJ-
+L---JF-JLJ.||-FJLJJ7
+|F|F-JF---7F7-L7L|7|
+|FFJF7L7F-JF7|JL---7
+7-L-JL7||F7|L7F-7F7|
+L.L7LFJ|||||FJL7||LJ
+L7JLJL-JLJLJL--JLJ.L\
+";
+
+// #┍┑┍S┍┑┍┑┍┑┍┑┍┑┍---┑
+// #|┕┙||||||||||||┍--┙
+// #┕-┑┕┙┕┙||||||┕┙┕-┑#
+// ┍--┙┍--┑||┕┙┕┙#┍┑┍┙#
+// ┕---┙┍-┙┕┙####┍┙┕┙##
+// ###┍-┙┍---┑###┕┑####
+// ##┍┙┍┑┕┑┍-┙┍┑##┕---┑
+// ##┕-┙┕┑||┍┑|┕┑┍-┑┍┑|
+// #####┍┙|||||┍┙┕┑||┕┙
+// #####┕-┙┕┙┕┙┕--┙┕┙##
+
+        assert_eq!(part_2(input), 10)
     }
 }
