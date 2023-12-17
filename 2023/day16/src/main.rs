e1888d8005b532803dfd9369636cd0d3c987cfeb
Peter Radziewicz <peterradziewicz@gmail.com>
2023-12-16 19:28:39 -0500

    WIP part 1 issue with cycle check

--- 2023/day16/src/main.rs
+++ 2023/day16/src/main.rs
@@ -10,7 +10,7 @@ const TEST: &str = r".|...\....
 .|....-|.\
 ..//.|....";
 
-#[derive(Debug, Default, Clone, Copy)]
+#[derive(Debug, Default, Clone, Copy, PartialEq, Eq)]
 enum Direction {
     Up,
     Down,
@@ -30,7 +30,7 @@ impl Direction {
     }
 }
 
-#[derive(Debug)]
+#[derive(Debug, PartialEq, Eq, Clone, Copy)]
 enum Kind {
     Space,
     RightMirror,
@@ -52,15 +52,24 @@ impl Kind {
     }
 }
 
-#[derive(Debug)]
+#[derive(Debug, PartialEq, Eq, Clone, Copy)]
 struct Tile {
     is_energized: bool,
     kind: Kind,
     coordinates: (usize, usize),
 }
 
+#[derive(Debug)]
+struct Beam {
+    complete: bool,
+    row: isize,
+    col: isize,
+    dir: Direction,
+    tiles: Vec<Tile>
+}
+
 fn main() {
-    println!("{}", part_1(INPUT));
+    println!("{}", part_1(TEST));
 }
 
 fn deserialize(input: &str) -> Vec<Vec<Tile>> {
@@ -82,67 +91,111 @@ fn deserialize(input: &str) -> Vec<Vec<Tile>> {
 
 fn part_1(input: &str) -> usize {
     let mut grid = deserialize(input);
-    let mut beams = vec![((0, -1), Direction::default())];
+    let mut beams = vec![Beam {
+        complete: false,
+        row: 0,
+        col: -1,
+        dir: Direction::default(),
+        tiles: Vec::new(),
+    }];
     let mut new_beams = Vec::new();
+
     loop {
+        beams.retain(|b| !b.complete);
+        if beams.len() == 0 {
+            break;
+        }
+        
+
         for beam in &mut beams {
-            println!("{beam:?}");
-            let (pos, dir) = beam;
-            let offset = dir.offset();
-            *pos = (pos.0 + offset.0, pos.1 + offset.1);
+            println!("{beam:?}\n");
+            let offset = beam.dir.offset();
 
-            if pos.0 < 0 || pos.0 >= grid.len() as isize {
+            beam.row = beam.row + offset.0;
+            beam.col = beam.col + offset.1;
+
+            if beam.row < 0 || beam.row >= grid.len() as isize {
+                beam.complete = true;
                 break;
             }
 
-            let row_idx = pos.0 as usize;
-            if pos.1 < 0 || pos.1 >= grid[row_idx].len() as isize {
+            let row_idx = beam.row as usize;
+
+            if beam.col < 0 || beam.col >= grid[row_idx].len() as isize {
+                beam.complete = true;
                 break;
             }
-   
-            let col_idx = pos.1 as usize;
+
+            let col_idx = beam.col as usize;
 
             let tile = &mut grid[row_idx][col_idx];
-            
-            println!("{tile:?}, Direction: {dir:?}");
+
+            // beam is cycling
+            if beam.tiles.contains(tile) {
+                println!("cycling, beam complete");
+                beam.complete = true;
+                break;
+            }
+
+            beam.tiles.push(tile.clone());
+            // println!("{tile:?}");
 
             tile.is_energized = true;
             match tile.kind {
                 Kind::Space => {}
-                Kind::RightMirror => match dir {
-                    Direction::Up => *dir = Direction::Right,
-                    Direction::Down => *dir = Direction::Left,
-                    Direction::Left => *dir = Direction::Down,
-                    Direction::Right => *dir = Direction::Up,
+                Kind::RightMirror => match beam.dir {
+                    Direction::Up => beam.dir = Direction::Right,
+                    Direction::Down => beam.dir = Direction::Left,
+                    Direction::Left => beam.dir = Direction::Down,
+                    Direction::Right => beam.dir = Direction::Up,
                 },
-                Kind::LeftMirror => match dir {
-                    Direction::Up => *dir = Direction::Left,
-                    Direction::Down => *dir = Direction::Right,
-                    Direction::Left => *dir = Direction::Up,
-                    Direction::Right => *dir = Direction::Down,
+                Kind::LeftMirror => match beam.dir {
+                    Direction::Up => beam.dir = Direction::Left,
+                    Direction::Down => beam.dir = Direction::Right,
+                    Direction::Left => beam.dir = Direction::Up,
+                    Direction::Right => beam.dir = Direction::Down,
                 },
-                Kind::HorizontalSplitter => match dir {
+                Kind::HorizontalSplitter => match beam.dir {
                     Direction::Up | Direction::Down => {
-                        new_beams.push(((pos.0, pos.1), Direction::Right));
-                        *dir = Direction::Left
+                        new_beams.push(Beam {
+                            complete: false,
+                            row: beam.row,
+                            col: beam.col,
+                            tiles: vec![tile.clone()],
+                            dir: Direction::Right,
+                        });
+                        beam.dir = Direction::Left
                     }
                     Direction::Left => {}
                     Direction::Right => {}
                 },
-                Kind::VerticalSplitter => match dir {
+                Kind::VerticalSplitter => match beam.dir {
                     Direction::Up => {}
                     Direction::Down => {}
                     Direction::Left | Direction::Right => {
-                        new_beams.push(((pos.0, pos.1), Direction::Up));
-                        *dir = Direction::Down
+                        new_beams.push(Beam {
+                            complete: false,
+                            row: beam.row,
+                            col: beam.col,
+                            tiles: vec![tile.clone()],
+                            dir: Direction::Up,
+                        });
+                        beam.dir = Direction::Down
                     }
                 },
             }
-
         }
         beams.extend(new_beams.drain(..))
     }
 
+    let mut count = 0;
+    for row in grid {
+        for tile in row {
+            if tile.is_energized {
+                count += 1;
+            }
+        }
+    }
 
-    usize::MIN
+    count
 }
