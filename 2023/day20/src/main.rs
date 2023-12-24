17a347940599dc0b7daccff561bf2cf61b94b1ef
Peter Radziewicz <peterradziewicz@gmail.com>
2023-12-24 14:21:11 -0500

    YOOO IT WORKS!

--- 2023/day20/src/main.rs
+++ 2023/day20/src/main.rs
@@ -1,4 +1,6 @@
 use std::collections::{HashMap, VecDeque};
+use std::fmt;
+use std::time::Instant;
 
 const INPUT: &str = include_str!("./input.txt");
 const TEST1: &str = "\
@@ -15,106 +17,182 @@ broadcaster -> a
 %b -> con
 &con -> output";
 
-#[derive(Debug, Clone, PartialEq, Eq)]
+trait Module: fmt::Debug {
+    fn pulse(&mut self, received: (String, PulseKind)) -> Vec<(String, PulseKind)>;
+    fn outputs(&self) -> Option<&[String]>;
+    fn inputs(&mut self) -> Option<&mut Vec<(String, PulseKind)>>;
+    fn counts(&self) -> (usize, usize);
+}
+
+#[derive(Debug, Clone, PartialEq, Eq, Default)]
 enum PulseKind {
+    #[default]
     Low,
     High,
 }
 
-#[derive(Debug, Clone)]
+#[derive(Debug, Clone, Default)]
 enum ModuleKind {
     FlipFlop,    // %
     Conjunction, // &
-    Broadcast,   // broadcaster
+    #[default]
+    Broadcast, // broadcaster
 }
 
-#[derive(Debug, Clone)]
+#[derive(Debug, Clone, Default)]
 struct PulseCount {
     low: usize,
     high: usize,
 }
 
-#[derive(Debug, Clone)]
-struct Module {
-    kind: ModuleKind,
-    // TODO: inputs can maybe just be the name and last pulse received
-    inputs: Option<Vec<String>>,
-    on: bool,
+#[derive(Debug)]
+struct Conjunction {
+    inputs: Vec<(String, PulseKind)>,
     outputs: Vec<String>,
     count: PulseCount,
-    pulse: Option<PulseKind>,
 }
 
-impl Module {
-    fn pulse(
-        &mut self,
-        received: Option<PulseKind>,
-        modules: &HashMap<String, Module>,
-    ) -> Option<PulseKind> {
-        match received {
-            Some(PulseKind::Low) => self.count.low += 1,
-            Some(PulseKind::High) => self.count.high += 1,
-            None => {}
+impl Module for Conjunction {
+    fn pulse(&mut self, received: (String, PulseKind)) -> Vec<(String, PulseKind)> {
+        match received.1 {
+            PulseKind::Low => self.count.low += 1,
+            PulseKind::High => self.count.high += 1,
         }
-        match self.kind {
-            ModuleKind::FlipFlop => match received {
-                Some(PulseKind::Low) => {
-                    let pulse = match self.on {
-                        true => {
-                            self.pulse = Some(PulseKind::Low);
-                            Some(PulseKind::Low)
-                        }
-                        false => {
-                            self.pulse = Some(PulseKind::High);
-                            Some(PulseKind::High)
-                        }
-                    };
-                    self.on = !self.on;
-                    pulse
-                }
-                _ => {
-                    self.pulse = None;
-                    None
-                }
-            },
-            ModuleKind::Conjunction => {
-                if self
-                    .inputs
-                    .as_ref()
-                    .unwrap()
+
+        let matched = self
+            .inputs
+            .iter_mut()
+            .find(|input| input.0 == received.0)
+            .unwrap();
+        matched.1 = received.1;
+
+        let kind = match self.inputs.iter().all(|(_, p)| p == &PulseKind::High) {
+            true => Some(PulseKind::Low),
+            false => Some(PulseKind::High),
+        };
+
+        self.outputs
+            .iter()
+            .map(|o| (o.clone(), kind.as_ref().unwrap().clone()))
+            .collect()
+    }
+    fn outputs(&self) -> Option<&[String]> {
+        Some(&self.outputs)
+    }
+    fn inputs(&mut self) -> Option<&mut Vec<(String, PulseKind)>> {
+        Some(&mut self.inputs)
+    }
+    fn counts(&self) -> (usize, usize) {
+        (self.count.low, self.count.high)
+    }
+}
+
+#[derive(Debug)]
+struct FlipFlop {
+    on: bool,
+    count: PulseCount,
+    outputs: Vec<String>,
+}
+
+impl Module for FlipFlop {
+    fn pulse(&mut self, received: (String, PulseKind)) -> Vec<(String, PulseKind)> {
+        match received.1 {
+            PulseKind::Low => {
+                self.count.low += 1;
+                let pulse = match self.on {
+                    true => Some(PulseKind::Low),
+                    false => Some(PulseKind::High),
+                };
+                self.on = !self.on;
+                self.outputs
                     .iter()
-                    .all(|i| modules.get(i).unwrap().pulse == Some(PulseKind::High))
-                {
-                    self.pulse = Some(PulseKind::Low);
-                    Some(PulseKind::Low)
-                } else {
-                    self.pulse = Some(PulseKind::High);
-                    return Some(PulseKind::High);
-                }
+                    .map(|o| (o.clone(), pulse.as_ref().unwrap().clone()))
+                    .collect()
             }
-            ModuleKind::Broadcast => {
-                self.pulse = Some(PulseKind::Low);
-                Some(PulseKind::Low)
+            PulseKind::High => {
+                self.count.high += 1;
+                vec![]
             }
         }
+    }
+    fn outputs(&self) -> Option<&[String]> {
+        Some(&self.outputs)
+    }
+    fn inputs(&mut self) -> Option<&mut Vec<(String, PulseKind)>> {
+        None
+    }
+    fn counts(&self) -> (usize, usize) {
+        (self.count.low, self.count.high)
+    }
+}
+
+#[derive(Debug)]
+struct Broadcast {
+    count: PulseCount,
+    outputs: Vec<String>,
+}
+
+impl Module for Broadcast {
+    fn pulse(&mut self, received: (String, PulseKind)) -> Vec<(String, PulseKind)> {
+        match received.1 {
+            PulseKind::Low => self.count.low += 1,
+            PulseKind::High => self.count.high += 1,
+        }
+        self.outputs
+            .iter()
+            .map(|o| (o.clone(), PulseKind::Low))
+            .collect()
+    }
+    fn outputs(&self) -> Option<&[String]> {
+        Some(&self.outputs)
+    }
+    fn inputs(&mut self) -> Option<&mut Vec<(String, PulseKind)>> {
+        None
+    }
+    fn counts(&self) -> (usize, usize) {
+        (self.count.low, self.count.high)
+    }
+}
+
+#[derive(Debug)]
+struct End {
+    count: PulseCount,
+    inputs: Vec<(String, PulseKind)>,
+}
+
+impl Module for End {
+    fn pulse(&mut self, received: (String, PulseKind)) -> Vec<(String, PulseKind)> {
+        match received.1 {
+            PulseKind::Low => self.count.low += 1,
+            PulseKind::High => self.count.high += 1,
+        }
+        vec![]
+    }
+    fn outputs(&self) -> Option<&[String]> {
+        None
+    }
+    fn inputs(&mut self) -> Option<&mut Vec<(String, PulseKind)>> {
+        Some(&mut self.inputs)
+    }
+    fn counts(&self) -> (usize, usize) {
+        (self.count.low, self.count.high)
     }
 }
 
 fn main() {
-    println!("Part 1: {}", part_1(INPUT));
+    let s = Instant::now();
+    println!(
+        "Part 1: {} in {:?}",
+        part_1(INPUT),
+        Instant::now().duration_since(s)
+    );
 }
 
 fn part_1(input: &str) -> usize {
-    let mut temp_modules: HashMap<String, Module> = HashMap::new();
+    let mut in_out = vec![];
+    let mut modules: HashMap<String, Box<dyn Module>> = HashMap::new();
+
     input.lines().for_each(|l| {
-        let mut module = Module {
-            kind: ModuleKind::Broadcast,
-            inputs: None,
-            on: false,
-            outputs: Vec::new(),
-            count: PulseCount { low: 0, high: 0 },
-            pulse: None,
-        };
         let (input, outputs) = l.split_once(" -> ").unwrap();
         let chars = input.chars().collect::<Vec<char>>();
         let (kind, name) = match chars[0] {
@@ -125,45 +203,87 @@ fn part_1(input: &str) -> usize {
             ),
             _ => (ModuleKind::Broadcast, input.to_string()),
         };
-        module.kind = kind;
-        module.outputs = outputs.split(", ").map(|s| s.to_string()).collect();
-        temp_modules.insert(name, module);
+        let outputs: Vec<String> = outputs.split(", ").map(String::from).collect();
+
+        let module: Box<dyn Module> = match kind {
+            ModuleKind::FlipFlop => Box::new(FlipFlop {
+                on: false,
+                count: PulseCount::default(),
+                outputs,
+            }),
+            ModuleKind::Conjunction => Box::new(Conjunction {
+                inputs: Vec::new(),
+                outputs,
+                count: PulseCount::default(),
+            }),
+            ModuleKind::Broadcast => Box::new(Broadcast {
+                count: PulseCount::default(),
+                outputs,
+            }),
+        };
+
+        in_out.push((name.clone(), module.outputs().unwrap().to_vec()));
+        modules.insert(name, module);
     });
 
-    let mut new_modules = temp_modules.clone();
-
-    for (name, module) in temp_modules {
-        for output in &module.outputs {
-            let output_module = new_modules.get_mut(output).unwrap();
-            match output_module.inputs {
-                Some(ref mut inputs) => inputs.push(name.to_string()),
-                None => output_module.inputs = Some(vec![name.to_string()]),
+    for (name, outputs) in in_out {
+        for output in outputs {
+            let output = modules.entry(output).or_insert(Box::new(End {
+                count: PulseCount::default(),
+                inputs: Vec::new(),
+            }));
+            if output.inputs().is_none() {
+                continue;
             }
+            output
+                .inputs()
+                .unwrap()
+                .push((name.clone(), PulseKind::Low));
         }
     }
 
-    for (name, module) in &new_modules {
-        println!("{}: {:?}", name, module);
+    
+    for _ in 0..1000 {
+        let broadcast = modules.get_mut("broadcaster").unwrap();
+        let mut states = VecDeque::new();
+        broadcast
+            .pulse((String::from("Button"), PulseKind::Low))
+            .iter()
+            .for_each(|(name, kind)| {
+                states.push_front(("broadcaster".to_string(), name.clone(), kind.clone()));
+            });
+    
+        while let Some((from, to, kind)) = states.pop_back() {
+            // println!("Processing state: FROM: {} TO: {} KIND: {:?}", from, to, kind);
+            let module = modules.get_mut(&to).unwrap();
+            let outputs = module.pulse((from.clone(), kind.clone()));
+            outputs.iter().for_each(|output| {
+                // println!("Adding state: {}: {:?}", output.0, output.1);
+                states.push_front((to.clone(), output.0.clone(), output.1.clone()));
+            });
+        }
+
     }
 
-    let mut broadcast = new_modules.get("broadcaster").unwrap().clone();
-    let pulse = broadcast.pulse(None, &new_modules);
-
-    let mut states = VecDeque::new();
-
-    for output in &broadcast.outputs {
-        states.push_front((pulse.clone(), output.to_string()));
+    let mut totals = (0_usize, 0_usize);
+    for (name, module) in &modules {
+        let counts = module.counts();
+        // println!("NAME: {} MODULE: {:?} TOTAL: {:?}", name, module, counts);
+        totals.0 += counts.0;
+        totals.1 += counts.1;
     }
 
-    // TODO: refactor this to not use new modules mutably and immutably for pulse
 
-    // while let Some((pulse, name)) = states.pop_back() {
-    //     let mut module = new_modules.get_mut(&name).unwrap();
-    //     let pulse = module.pulse(pulse, &new_modules);
-    //     for output in &module.outputs {
-    //         states.push_front((pulse.clone(), output.to_string()));
-    //     }
-    // }
+    totals.0 * totals.1
+}
 
-    0
+#[cfg(test)]
+mod tests{
+    use super::*;
+
+    #[test]
+    fn test_part_1() {
+        assert_eq!(part_1(TEST1), 32000000);
+        assert_eq!(part_1(TEST2), 11687500);
+    }
 }
