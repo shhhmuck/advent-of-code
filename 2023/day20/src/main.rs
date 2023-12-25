2025dea5d59a15f37a8875ed2e566a74797c8a28
Peter Radziewicz <peterradziewicz@gmail.com>
2023-12-24 20:07:44 -0500

    part 2 solved

--- 2023/day20/src/main.rs
+++ 2023/day20/src/main.rs
@@ -2,20 +2,9 @@ use std::collections::{HashMap, VecDeque};
 use std::fmt;
 use std::time::Instant;
 
-const INPUT: &str = include_str!("./input.txt");
-const TEST1: &str = "\
-broadcaster -> a, b, c
-%a -> b
-%b -> c
-%c -> inv
-&inv -> a";
+use num::integer::lcm;
 
-const TEST2: &str = "\
-broadcaster -> a
-%a -> inv, con
-&inv -> b
-%b -> con
-&con -> output";
+const INPUT: &str = include_str!("./input.txt");
 
 trait Module: fmt::Debug {
     fn pulse(&mut self, received: (String, PulseKind)) -> Vec<(String, PulseKind)>;
@@ -186,6 +175,119 @@ fn main() {
         part_1(INPUT),
         Instant::now().duration_since(s)
     );
+    let s = Instant::now();
+    println!(
+        "Part 2: {} in {:?}",
+        part_2(INPUT),
+        Instant::now().duration_since(s)
+    );
+}
+
+fn part_2(input: &str) -> usize {
+    let mut in_out = vec![];
+    let mut modules: HashMap<String, Box<dyn Module>> = HashMap::new();
+
+    input.lines().for_each(|l| {
+        let (input, outputs) = l.split_once(" -> ").unwrap();
+        let chars = input.chars().collect::<Vec<char>>();
+        let (kind, name) = match chars[0] {
+            '%' => (ModuleKind::FlipFlop, chars[1..].iter().collect::<String>()),
+            '&' => (
+                ModuleKind::Conjunction,
+                chars[1..].iter().collect::<String>(),
+            ),
+            _ => (ModuleKind::Broadcast, input.to_string()),
+        };
+        let outputs: Vec<String> = outputs.split(", ").map(String::from).collect();
+
+        let module: Box<dyn Module> = match kind {
+            ModuleKind::FlipFlop => Box::new(FlipFlop {
+                on: false,
+                count: PulseCount::default(),
+                outputs,
+            }),
+            ModuleKind::Conjunction => Box::new(Conjunction {
+                inputs: Vec::new(),
+                outputs,
+                count: PulseCount::default(),
+            }),
+            ModuleKind::Broadcast => Box::new(Broadcast {
+                count: PulseCount::default(),
+                outputs,
+            }),
+        };
+
+        in_out.push((name.clone(), module.outputs().unwrap().to_vec()));
+        modules.insert(name, module);
+    });
+
+    for (name, outputs) in in_out {
+        for output in outputs {
+            let output = modules.entry(output).or_insert(Box::new(End {
+                count: PulseCount::default(),
+                inputs: Vec::new(),
+            }));
+            if output.inputs().is_none() {
+                continue;
+            }
+            output
+                .inputs()
+                .unwrap()
+                .push((name.clone(), PulseKind::Low));
+        }
+    }
+
+    let mut cycles: Vec<Vec<usize>> = vec![vec![]; 4];
+
+    let mut idx = 0;
+    loop {
+        if cycles.iter().all(|c| c.len() >= 2) {
+            println!("Broke at {idx}");
+            break;
+        }
+
+        let broadcast = modules.get_mut("broadcaster").unwrap();
+        let mut states = VecDeque::new();
+
+        broadcast
+            .pulse((String::from("button"), PulseKind::Low))
+            .iter()
+            .for_each(|(name, kind)| {
+                states.push_front(("broadcaster".to_string(), name.clone(), kind.clone()));
+            });
+
+        while let Some((from, to, kind)) = states.pop_back() {
+            // dr is the only input to rx, and its a conjunction with 4 inputs
+            // all need to be high to pulse low so lets find their cycles
+            if to == "dr" && kind == PulseKind::High {
+                match from.as_str() {
+                    "mp" => cycles[0].push(idx),
+                    "qt" => cycles[1].push(idx),
+                    "qb" => cycles[2].push(idx),
+                    "ng" => cycles[3].push(idx),
+                    _ => unreachable!(),
+                }
+            }
+
+            let module = modules.get_mut(&to).unwrap();
+            let outputs = module.pulse((from.clone(), kind.clone()));
+            outputs.iter().for_each(|output| {
+                // println!("Adding state: {}: {:?}", output.0, output.1);
+                states.push_front((to.clone(), output.0.clone(), output.1.clone()));
+            });
+        }
+
+        idx += 1;
+    }
+
+    let mut calcs = vec![];
+
+    // calc the cycle length which we can use for lcm
+    for cycle in cycles {
+        calcs.push(cycle[1] - cycle[0]);
+    }
+
+    calcs.iter().fold(1, |acc, x| lcm(acc, *x))
 }
 
 fn part_1(input: &str) -> usize {
@@ -242,17 +344,17 @@ fn part_1(input: &str) -> usize {
         }
     }
 
-    
     for _ in 0..1000 {
         let broadcast = modules.get_mut("broadcaster").unwrap();
         let mut states = VecDeque::new();
+
         broadcast
-            .pulse((String::from("Button"), PulseKind::Low))
+            .pulse((String::from("button"), PulseKind::Low))
             .iter()
             .for_each(|(name, kind)| {
                 states.push_front(("broadcaster".to_string(), name.clone(), kind.clone()));
             });
-    
+
         while let Some((from, to, kind)) = states.pop_back() {
             // println!("Processing state: FROM: {} TO: {} KIND: {:?}", from, to, kind);
             let module = modules.get_mut(&to).unwrap();
@@ -262,28 +364,43 @@ fn part_1(input: &str) -> usize {
                 states.push_front((to.clone(), output.0.clone(), output.1.clone()));
             });
         }
-
     }
 
     let mut totals = (0_usize, 0_usize);
-    for (name, module) in &modules {
+    for module in modules.values() {
         let counts = module.counts();
-        // println!("NAME: {} MODULE: {:?} TOTAL: {:?}", name, module, counts);
         totals.0 += counts.0;
         totals.1 += counts.1;
     }
-
 
     totals.0 * totals.1
 }
 
 #[cfg(test)]
-mod tests{
+mod tests {
     use super::*;
 
+    const TEST1: &str = "\
+broadcaster -> a, b, c
+%a -> b
+%b -> c
+%c -> inv
+&inv -> a";
+
+    const TEST2: &str = "\
+broadcaster -> a
+%a -> inv, con
+&inv -> b
+%b -> con
+&con -> output";
+
     #[test]
-    fn test_part_1() {
+    fn test_part_1_first() {
         assert_eq!(part_1(TEST1), 32000000);
+    }
+
+    #[test]
+    fn test_part_1_second() {
         assert_eq!(part_1(TEST2), 11687500);
     }
 }
