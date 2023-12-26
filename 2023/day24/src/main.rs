e17b77c7f91ad80876ba34fcaa920a63faa6f526
Peter Radziewicz <peterradziewicz@gmail.com>
2023-12-25 19:54:13 -0500

    part 1 done

--- 2023/day24/src/main.rs
+++ 2023/day24/src/main.rs
@@ -1,7 +1,10 @@
+use std::time::Instant;
+
 const INPUT: &str = include_str!("./input.txt");
 
 fn main() {
-    part_1(INPUT);
+    let s = Instant::now();
+    println!("Part 1: {} in {:?}", part_1(INPUT, (200000000000000.0, 400000000000000.0)), Instant::now().duration_since(s));
 }
 
 #[derive(Debug)]
@@ -55,67 +58,48 @@ impl Hailstone {
     }
 
     fn overlap_position(&self, other: &Hailstone) -> Option<Position> {
-        if let Some(time) = self.overlap_time(other) {
-            let x_self = self.position.x + self.velocity.x * time;
-            let y_self = self.position.y + self.velocity.y * time;
-            // let z_self = self.position.z + self.velocity.z * time;
-            let x_other = other.position.x + other.velocity.x * time;
-            let y_other = other.position.y + other.velocity.y * time;
-            // let z_other = other.position.z + other.velocity.z * time;
-
-            let x = (x_self + x_other) / 2.0;
-            let y = (y_self + y_other) / 2.0;
-            // let z = (z_self + z_other) / 2.0;
-
-            Some(Position::new(x, y, 0.0))
-        } else {
-            None
-        }
-    }
-
-    fn overlap_time(&self, other: &Hailstone) -> Option<f64> {
-        let div_x = other.velocity.x - self.velocity.x;
-        let div_y = other.velocity.y - self.velocity.y;
-        // let div_z = other.velocity.z - self.velocity.z;
-
-        // Check if velocities are parallel or hailstones never intersect
-        if div_x.abs() < f64::EPSILON || div_y.abs() < f64::EPSILON
-        /*|| div_z.abs() < f64::EPSILON*/
-        {
+        let det = (self.velocity.x * other.velocity.y) - (self.velocity.y * other.velocity.x);
+        if det == 0.0 {
             return None;
         }
-
-        let time_x = (self.position.x - other.position.x) / div_x;
-        let time_y = (self.position.y - other.position.y) / div_y;
-        // let time_z = (self.position.z - other.position.z) / div_z;
-
-        // Select maximum non-negative time for intersection
-        let max_time = [time_x, time_y /*time_z*/]
-            .iter()
-            .filter(|&t| *t >= 0.0)
-            .cloned()
-            .fold(f64::NEG_INFINITY, f64::max);
-
-        if max_time.is_infinite() {
-            None
-        } else {
-            Some(max_time)
+        let time: f64 = ((other.position.x - self.position.x) * other.velocity.y
+            - (other.position.y - self.position.y) * other.velocity.x)
+            as f64
+            / det;
+        let time2: f64 = ((self.position.x - other.position.x) * self.velocity.y
+            - (self.position.y - other.position.y) * self.velocity.x)
+            as f64
+            / det;
+        if time < 0.0 {
+            return None;
         }
+        if time * time2 > 0.0 {
+            return None;
+        }
+        let x = self.position.x as f64 + self.velocity.x as f64 * time;
+        let y = self.position.y as f64 + self.velocity.y as f64 * time;
+
+        Some(Position { x, y, z: 0.0 })
     }
 }
 
-fn part_1(input: &str) -> usize {
+fn part_1(input: &str, range: (f64, f64)) -> usize {
     let hailstones = input
         .lines()
         .map(Hailstone::from_line)
         .collect::<Vec<Hailstone>>();
 
+    get_overlapped_within_range(&hailstones, range)
+}
+
+fn get_overlapped_within_range(hailstones: &[Hailstone], allowed_range: (f64, f64)) -> usize {
     let mut overlapped_within_range = 0;
+    let (min, max) = allowed_range;
 
     for i in 0..hailstones.len() {
         for j in i + 1..hailstones.len() {
             if let Some(Position { x, y, z: _ }) = hailstones[i].overlap_position(&hailstones[j]) {
-                if x > 7.0 && x < 27.0 && y > 7.0 && y < 27.0 {
+                if x > min && x < max && y > min && y < max {
                     overlapped_within_range += 1;
                 }
             }
@@ -136,6 +120,12 @@ mod tests {
 12, 31, 28 @ -1, -2, -1
 20, 19, 15 @ 1, -5, -3";
 
+    // round the values from overlap function to 3 decimal places to match example tests rounding
+    fn round_to_precision(x: f64, precision: u32) -> f64 {
+        let multiplier = 10f64.powi(precision as i32);
+        (x * multiplier).round() / multiplier
+    }
+
     #[test]
     fn test_overlap_1() {
         let hailstone_1 = Hailstone::from_line("19, 13, 30 @ -2, 1, -2");
@@ -143,8 +133,8 @@ mod tests {
 
         let res = hailstone_1.overlap_position(&hailstone_2).unwrap();
 
-        assert_eq!(res.x, 14.333);
-        assert_eq!(res.y, 15.333);
+        assert_eq!(round_to_precision(res.x, 3), 14.333);
+        assert_eq!(round_to_precision(res.y, 3), 15.333);
     }
 
     #[test]
@@ -154,12 +144,12 @@ mod tests {
 
         let res = hailstone_1.overlap_position(&hailstone_2).unwrap();
 
-        assert_eq!(res.x, 11.667);
-        assert_eq!(res.y, 16.667);
+        assert_eq!(round_to_precision(res.x, 3), 11.667);
+        assert_eq!(round_to_precision(res.y, 3), 16.667);
     }
 
     #[test]
     fn it_works() {
-        assert_eq!(part_1(TEST), 2);
+        assert_eq!(part_1(TEST, (7.0, 27.0)), 2);
     }
 }
